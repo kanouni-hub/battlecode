389869d47fd9b42fb824531e222de414867d450f
mostapha-essollami <59730052+mostapha-essollami@users.noreply.github.com>
2020-01-12 14:06:34 +0100

    Update Astar.java

--- Astar.java
+++ Astar.java
@@ -1,84 +1,90 @@
 package examplefuncsplayer;
+
 import battlecode.common.*;
+
 
 public abstract class Astar extends RobotPlayer  {
 	static MapLocation s;
 	public static MapLocation[] ArrayLocation(MapLocation a) {
 		
-	return new MapLocation[8];}
+	return Miner.getadj(a);}
 	
 	public static void RAstar() throws GameActionException {
-		   
+		   System.out.println("obj "+s);
 			MapLocation me=rc.getLocation();
 			MapLocation[] A=ArrayLocation(me);
+			
 
 		
 			Direction dir = me.directionTo(s);
 			MapLocation h=me.add(dir);
-			if(rc.canMove(dir)) {
-				try {
+			if(h==s) { System.out.println("h "+h);
+			System.out.println("s "+s);
+			return ; }
+			
+			if(rc.canMove(dir) && !rc.senseFlooding(h)) {
+				try {System.out.println("here");
 					rc.move(dir);
 				} catch (GameActionException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				}Clock.yield();
+				//RAstar();	
 				}
-				Clock.yield();	}
 	// if the robot cant move initially	move
 			
 				
 			//look if it can't move because of enemy robot ;
 			
 	else {		// if true detour ;
-					if(rc.senseRobotAtLocation(h)!=null || rc.senseFlooding(h)) {
-	
 					for(MapLocation i :A) {
-						if(rc.canMove(me.directionTo(i))) {
+						if(rc.canMove(me.directionTo(i)) && (!rc.senseFlooding(me.add(me.directionTo(i))))) {
+							System.out.println("here2");
 							try {
-								rc.move(dir);
+								rc.move(me.directionTo(i));
 							} catch (GameActionException e) {
 								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
+								e.printStackTrace();}
 							
 							Clock.yield();
-							break;}
+						//	RAstar();
+							
 					}}
 				//If its something else ''elevation''
-				else { //If it's a Landcaper forceyour way in;
-				        if(rc.getType()== RobotType.LANDSCAPER) {
+				 //If it's a Landcaper forceyour way in;
+				       /* if(rc.getType()== RobotType.LANDSCAPER) {
 					        while(rc.canDigDirt(dir) && (!rc.canMove(dir))) {try {
-						        rc.digDirt(dir);} catch (GameActionException e) {e.printStackTrace();}}
+						        rc.digDirt(dir);} catch (GameActionException e) {e.printStackTrace();}RAstar();}
 					        
-					        if(rc.canMove(dir)) {
+					        if(rc.canMove(dir)&&(!rc.senseFlooding(h))) {
 					        		try {rc.move(dir);} catch (GameActionException e) {
-					        			e.printStackTrace();}Clock.yield();}
+					        			e.printStackTrace();}Clock.yield();RAstar();}
 					        
 					        else {       if(rc.canDepositDirt(dir.opposite())) 
 					        				{try {rc.depositDirt(dir);} catch (GameActionException e) 
-					        				{ e.printStackTrace();}Clock.yield();}
+					        				{ e.printStackTrace();}Clock.yield();RAstar();}*/
 					
-					    else {//IF IT'S NOT A LANDSCAPER DETOUR
+					   /* else {//IF IT'S NOT A LANDSCAPER DETOUR
 					    	for(MapLocation i :A) {
-								if(rc.canMove(me.directionTo(i))) {
+								if(rc.canMove(me.directionTo(i))&&(!rc.senseFlooding(me.add(me.directionTo(i))))) {
 									try {
 										rc.move(dir);
 									} catch (GameActionException e) {
 										// TODO Auto-generated catch block
 										e.printStackTrace();
 									}
-									
-									Clock.yield();
-									break;}
+								Clock.yield();	
+								RAstar();
+									}*/
 					    }
-					        }}
+			RAstar();
+					        }
 				
 			
-		}
+		
 			
-		}
-
-	}}}
+		
 
 	
-
-
+			}
+	
