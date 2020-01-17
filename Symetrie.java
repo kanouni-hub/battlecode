0483a29de35e56f32614c8dba0259f27ca25e32b
kanouni-hub <59743025+kanouni-hub@users.noreply.github.com>
2020-01-17 22:59:32 +0100

    Update Symetrie.java

--- Symetrie.java
+++ Symetrie.java
@@ -8,25 +8,25 @@ public class Symetrie extends RobotPlayer{
 	int a ;
 	
 	// symetrie x
-	public MapLocation verticale( MapLocation h) {
+	public static MapLocation verticale( MapLocation h) {
 		int xprime;
 		int a = rc.getMapWidth() - 2*(h.x) ;
-		int u = h.x +a;
+		int u = h.x +a-1;
 		if(u>rc.getMapWidth()) {
-		 	xprime = h.x -a ;}
+		 	xprime = h.x -a-1 ;}
 		
 		else { xprime =u;}
 		
 		MapLocation hprime = new MapLocation(xprime,h.y);
 		return hprime ;}
 	// symetrie  y
-	public MapLocation horizantal( MapLocation h) {
+	public static MapLocation horizantal( MapLocation h) {
 		int yprime;
 		int a = rc.getMapHeight() - 2*(h.y) ;
-		int u = h.y +a;
+		int u = h.y +a-1;
 		
 		if(u>rc.getMapHeight()) {
-		 	yprime = h.y -a ;}
+		 	yprime = h.y -a-1 ;}
 		
 		else {  yprime =u;}
 		
@@ -34,12 +34,12 @@ public class Symetrie extends RobotPlayer{
 		return hprime ; }
 	
 	// symetrie  y=x
-	public MapLocation Diagonale(MapLocation h) {
+	public static MapLocation Diagonale(MapLocation h) {
 			MapLocation hprime = new MapLocation(h.y,h.x);
 			return hprime ;}
 	
 	// symetrie  inverse diagonale
-	public MapLocation InvDiagonale(MapLocation h) {
+	public static MapLocation InvDiagonale(MapLocation h) {
 		int yprime;
 		int xprime ;
 		int b = rc.getMapHeight()*(1-(h.x)/rc.getMapWidth());
@@ -52,23 +52,28 @@ public class Symetrie extends RobotPlayer{
 		MapLocation hprime = new MapLocation(xprime,yprime);
 		return hprime ;}
 		
-	public boolean FindHQ(MapLocation h) {
+	public static boolean FindHQ(MapLocation h) {
 			
 			
-			MapLocation[] Possible = {verticale(h), horizantal(h),Diagonale(h),InvDiagonale(h)};
+			MapLocation[] Possible = {verticale(h), Diagonale(h),horizantal(h),horizantal(verticale(h))};
 			for(MapLocation u : Possible) {
-					try {
+				System.out.println("u "+u);
+				try {
 					Pathfind.going(u);
+					System.out.println("myHQ" +u);
 				} catch (GameActionException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				
 				try {
-					if ( (rc.senseRobotAtLocation(u)).getType()==RobotType.HQ){
-						 
-						 break;
-						}} catch (GameActionException e) {
+					if ( (rc.senseRobotAtLocation(u))!=null){
+						if(rc.senseRobotAtLocation(u).getType()==RobotType.HQ && rc.senseRobotAtLocation(u).getTeam()!=rc.getTeam()) {
+							opHQ=u;
+							break ;
+						}}
+				
+						} catch (GameActionException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();}
 				
@@ -78,3 +83,4 @@ public class Symetrie extends RobotPlayer{
 			return true ;}
 
 }
+
