3306168df9c94a672b32a03ea728ee1353cb4b9d
mostapha-essollami <59730052+mostapha-essollami@users.noreply.github.com>
2020-01-18 12:17:18 +0100

    Add files via upload

--- Pathfind.java
+++ Pathfind.java
@@ -1,55 +1,85 @@
 package examplefuncsplayer;
 
+import java.util.LinkedList;
+
 import battlecode.common.*;
 
 public class Pathfind extends Global{
 
-	static void going(MapLocation goal) throws GameActionException {
-		
+	static boolean going(MapLocation goal) throws GameActionException {
+		System.out.println("going to "+goal);
+		int nbrpas=0;
+		myloc=rc.getLocation();
+		int maxpas=(int)Math.sqrt(myloc.distanceSquaredTo(goal));
+		maxpas=maxpas*3;		
+		System.out.println(" pasmx "+maxpas);
 		boolean moved=false;
 		boolean foundborder=false;
+		LinkedList<MapLocation> visited3=new LinkedList<MapLocation>();
+		visited3.clear();
+		visited3.add(myloc);
+		visited3.add(myloc);
+		visited3.add(myloc);
+		Direction prevdir=Direction.CENTER;
+		int co=0;
 		while(!rc.getLocation().isAdjacentTo(goal)) {
 		myloc=rc.getLocation();
 		Direction forward=myloc.directionTo(goal);
-		if(rc.canMove(forward)) {
+		while(!rc.isReady()) {}
+		if(co!=5 && rc.canMove(forward) && !visited3.contains(myloc.translate(forward.getDeltaX(),forward.getDeltaY())) && !rc.senseFlooding(myloc.translate(forward.getDeltaX(),forward.getDeltaY()))) {
 			moved=true;
 			rc.move(forward);
+			nbrpas++;
+			prevdir=forward.opposite();
+			visited3.pop();
+			visited3.add(rc.getLocation());
 			Clock.yield();
-
+		
 		}
 		else {
-			int co=0;
-			while(!moved && !foundborder) {
+			co=0;
+			while(!moved && !foundborder && !prevdir.equals(forward)) {
 				forward=forward.rotateLeft();
 				co++;
-				if(rc.canMove(forward)) {
-					System.out.println(co+" left "+forward);
+				if(rc.canMove(forward) && !rc.senseFlooding(myloc.translate(forward.getDeltaX(),forward.getDeltaY()))) {
 
 					moved=true;
 					rc.move(forward);
-					
+					nbrpas++;
+					prevdir=forward.opposite();
+
+					visited3.pop();
+					visited3.add(rc.getLocation());
 					Clock.yield();
 					break;
 				}else 	if(!rc.onTheMap(myloc.translate(forward.getDeltaX(),forward.getDeltaY()))) {
-					System.out.println("border");
 				foundborder=true;
 			}
 			}
-			while(!moved && foundborder) {
+			if(prevdir.equals(forward)) {prevdir=Direction.CENTER; foundborder=!foundborder;}
+			while(!moved && foundborder ) {
 				forward=forward.rotateRight();
-				System.out.println("right");
-				if(rc.canMove(forward)) {
+				if(rc.canMove(forward) && !rc.senseFlooding(myloc.translate(forward.getDeltaX(),forward.getDeltaY()))) {
 					moved=true;
 					rc.move(forward);
+					nbrpas++;
+					prevdir=forward.opposite();
+					visited3.pop();
+					visited3.add(rc.getLocation());
 					Clock.yield();
 					break;
 				}else 	if(!rc.onTheMap(myloc.translate(forward.getDeltaX(),forward.getDeltaY()))) {
-					System.out.println("border");
 				foundborder=false;
 			}
 			}
+			
 			moved=false;
+			
+			
+		}	if(nbrpas>maxpas) {
+			return false;
 		}
 		}
+		return true;
 	}
 }
