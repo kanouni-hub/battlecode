bbcf7248a7cfeb3738ec18d0da29a78e729274ff
kanouni-hub <59743025+kanouni-hub@users.noreply.github.com>
2020-01-22 20:32:51 +0100

    Update Soupbaby.java

--- Soupbaby.java
+++ Soupbaby.java
@@ -1,11 +1,11 @@
-package examplefuncsplayer;
+package test;
 import battlecode.common.*;
 
 public class Soupbaby extends Global {
 
 public static void Mine() throws GameActionException {
 		
-		
+
 		
 		MapLocation[] h=rc.senseNearbySoup();
 		for(MapLocation u : h) {
@@ -13,12 +13,12 @@ public static void Mine() throws GameActionException {
 			
 			MapLocation me = rc.getLocation();
 			Direction dir = me.directionTo(u);
-			System.out.println("u"+u);
+		//	System.out.println("u"+u);
 			while(rc.senseSoup(u)!= 0 && rc.getSoupCarrying()<100) {
 			if(rc.canMineSoup(dir)) {
 				
 					rc.mineSoup(dir);
-					System.out.println("minning soup"+rc.getSoupCarrying());
+			//		System.out.println("minning soup"+rc.getSoupCarrying());
 				} 
 				}
 		}
@@ -30,13 +30,38 @@ public static void Mine() throws GameActionException {
 			
 		}
 				while(rc.getSoupCarrying()!=0) {
+				Message.refineryLocation();
+				if(refinery==null) {
 				if(Pathfind.going(myHQ)) {
 				MapLocation futreme = rc.getLocation();
 				Direction futurdir = futreme.directionTo(myHQ);
 				if(rc.canDepositSoup(futurdir)) {
 					rc.depositSoup(futurdir,rc.getSoupCarrying());
 					
-				}}
+				}
+				else{
+					if(rc.canBuildRobot(RobotType.REFINERY,myHQ.directionTo(rc.getLocation()))) {
+						rc.buildRobot(RobotType.REFINERY,myHQ.directionTo(rc.getLocation()));
+						refinery = rc.adjacentLocation(myHQ.directionTo(rc.getLocation()));
+						Message.broadcastrefinery();
+					}
+					
+					
+					}
+				}
+				else {
+					Pathfind.going(refinery);
+					MapLocation futreme = rc.getLocation();
+					Direction futurdir = futreme.directionTo(myHQ);
+					if(rc.canDepositSoup(futurdir)) {
+						rc.depositSoup(futurdir,rc.getSoupCarrying());
+						
+						
+					}
+					
+					
+				}
+				}
 				}return;
 					
 			} 
@@ -48,15 +73,4 @@ public static void Mine() throws GameActionException {
 		
 				
 		
-	public static void getsoup() throws GameActionException {
-		Miner.firstmove();
-		MapLocation[] h = rc.senseNearbySoup();
-		Pathfind.going(h[0]);
-		Mine();
-			
-	getsoup();}
-		
-	
 }
-
-
