9d5ef578e8e3d53a995c8d67ef21cf3cc8c022e4
kanouni-hub <59743025+kanouni-hub@users.noreply.github.com>
2020-01-23 21:14:41 +0100

    Update Message.java

--- Message.java
+++ Message.java
@@ -32,30 +32,18 @@ import battlecode.common.*;
 			}
 	}
 	//HQ attaqué
-	public static void ennemy() throws GameActionException {
-		RobotInfo[] Robots=rc.senseNearbyRobots();
-		for(RobotInfo rb:Robots) {
-			if(rb.getTeam()!=rc.getTeam()) {
-				 switch (rb.getType()){
-				 case LANDSCAPER: 
-					 int [] message = new int[7] ;
-					 message[0]=code;
-					 message[1]=10;
-					 if(rc.canSubmitTransaction(message, 2)) {
-							rc.submitTransaction(message, 2);
-						}
-					 
-				 case DELIVERY_DRONE:
-					 int[] message1= new int[7];
-					 message1[0]=code;
-					 message1[1]=11;
-					 if(rc.canSubmitTransaction(message1, 2)) {
-							rc.submitTransaction(message1, 2);
-						}
-					 
-				}
+
+	public static void attackennemy() throws GameActionException {
+		if(rc.getDirtCarrying()!=0) {
+			int [] message = new int[7];
+			message[0]=code;
+			message[1]=12;
+			message[4]=1;
+			if(rc.canSubmitTransaction(message, 5)) {
+				rc.submitTransaction(message, 5);
 			}
 		}
+
 		
 	}
 	//refinery 
@@ -74,7 +62,7 @@ import battlecode.common.*;
 	// look if there is landscaper
 	public static void Wall() throws GameActionException {
 		int c=0;
-		for(RobotInfo robot:rc.senseNearbyRobots(1)) {
+		for(RobotInfo robot:rc.senseNearbyRobots(2)) {
 			if(robot.getTeam()==rc.getTeam()) {
 				if(robot.getType()==RobotType.LANDSCAPER) {
 					c=+1;
@@ -101,6 +89,44 @@ import battlecode.common.*;
 			
 		}
 		}
+	//get coordination
+	public static void Coordination() throws GameActionException {
+		if(rc.getTeamSoup()>=220) {
+			int[] message= new int[7];
+			message[0]=code;
+			message[1]=5;
+			message[4]=1;
+			if(rc.canSubmitTransaction(message, 2)) {
+				rc.submitTransaction(message, 2);
+			}
+		}
+	}
+	
+	// if the design school is under attaque
+	public static void nodesign() throws GameActionException {
+		if(rc.getDirtCarrying()>=5) {
+			MapLocation Designer = rc.getLocation();
+			int[] message= new int[7];
+			message[0]=code;
+			message[1]=6;
+			message[4]=1;
+			message[5]=Designer.x;
+			message[6]=Designer.y;
+			if(rc.canSubmitTransaction(message, 2)) {
+				rc.submitTransaction(message, 2);
+			}
+		}
+		else {
+			int[] message= new int[7];
+			message[0]=code;
+			message[1]=6;
+			message[4]=0;
+			if(rc.canSubmitTransaction(message, 2)) {
+				rc.submitTransaction(message, 2);
+			}
+		}
+		}
+		
 	
 
 	// prendre la location du HQ
@@ -110,6 +136,7 @@ import battlecode.common.*;
 				int [] mess =tx.getMessage();
 				if(mess[0]==code &&mess[1]==1) {
 					myHQ= new MapLocation(mess[4],mess[5]);
+					return;
 				}
 				
 			}
@@ -122,6 +149,7 @@ import battlecode.common.*;
 				int [] mess =tx.getMessage();
 				if(mess[0]==code &&mess[1]==2) {
 					opHQ= new MapLocation(mess[4],mess[5]);
+					return;
 				}
 				
 			}
@@ -134,10 +162,10 @@ import battlecode.common.*;
 				int [] mess =tx.getMessage();
 				if(mess[0]==code &&mess[1]==3) {
 					 refinery= new MapLocation(mess[4],mess[5]);
+					 return;
 				}
 				
-			}
-		}
+			}}
 	}
 	// Is there a wall built
 	public static void builtwall() throws GameActionException {
@@ -146,11 +174,55 @@ import battlecode.common.*;
 				int [] mess =tx.getMessage();
 				if(mess[0]==code &&mess[1]==4) {
 					pr = mess[4];
-				}
+				}	
 				
 			}
 		}
 	}
+	//getcoordination 
+	public static void getCoordination() throws GameActionException {
+		int i = rc.getRoundNum();
+		for(Transaction tx : rc.getBlock(i) ) {
+				int [] mess = tx.getMessage();
+				if(mess[0]==code &&mess[1]==5) {
+					build = mess[4];
+					break;
+				}
+				
+			}
+		}
+	//message urgent 
+	public static void urgentmess() throws GameActionException {
+		int i = rc.getRoundNum();
+		for(Transaction tx : rc.getBlock(i) ) {
+				int [] mess = tx.getMessage();
+				if(mess[0]==code &&mess[1]==12) {
+					urgent = mess[4];
+					break;
+				}
+				
+			}
 	}
+	//get message about the attaque on the design school 
+	public static MapLocation getdesign() throws GameActionException {
+		int i = rc.getRoundNum();
+		MapLocation goal;
+		int x=0 ;
+		int y=0;
+		for(Transaction tx : rc.getBlock(i) ) {
+				int [] mess = tx.getMessage();
+				if(mess[0]==code &&mess[1]==6) {
+					design = mess[4];
+					x=mess[5];
+					y=mess[6];
+					break;
+				}
+				
+			}
+		goal=new MapLocation(x,y);
+		return(goal);}
+	}
+	
+	
 
 
